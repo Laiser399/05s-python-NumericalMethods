d0813a2f20c080f96df621aa873aa2b4792f38a2
Laiser399 <Laiser399@mail.ru>
2019-09-20 23:21:55 +0300

    simple iteration done without use error

--- lab02.py
+++ lab02.py
@@ -10,6 +10,9 @@ error = 10 ** -5
 def func(x: float):
     return 5*x**3 + 2*x**2 - 15*x - 6
 
+def func_derivative(x: float):
+    return 15*x**2 + 4*x - 15
+
 def dichotomy_search(func, a: float, b: float, error: float):
     if a >= b or func(a) * func(b) >= 0 or error <= 0:
         raise ValueError
@@ -25,8 +28,28 @@ def dichotomy_search(func, a: float, b: float, error: float):
             b = center
     return (a + b) / 2
 
-print(dichotomy_search(func, -2, -1, error))
-print(dichotomy_search(func, -1, 0, error))
-print(dichotomy_search(func, 0, 2, error))
+
+def simple_iteration(func, t: float, x_0: float, n: int):
+    x = x_0
+    for i in range(n):
+        x = x + t * func(x)
+    return x
+
+
+res1 = [
+    dichotomy_search(func, -2, -1, error),
+    dichotomy_search(func, -1, 0, error),
+    dichotomy_search(func, 0, 2, error),
+]
+
+res2 = [
+    simple_iteration(func, -2 / (func_derivative(-2) + func_derivative(-1.5)), -1.9, 20),
+    simple_iteration(func, -2 / (func_derivative(-1) + func_derivative(0)), -0.5, 20),
+    simple_iteration(func, -2 / (func_derivative(1) + func_derivative(2)), 1.5, 20)
+]
+
+print(res1)
+print(res2)
+
 
 
